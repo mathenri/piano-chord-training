9c316213f458f5e7855a3f42458950a92d28ee49
Mattias Henriksson <mattias.henriksson@readly.com>
2022-02-14 14:34:40 +0100

    reafactor: use react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { withCookies, Cookies } from 'react-cookie';
 import './App.css';
 import { CHORDS, CHORD_FAMILIES } from './chords.js'
 import { PIANO_KEYS } from './pianoKeys.js'
@@ -10,185 +11,159 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 
 
-class App extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      selectedPianoKeys: [],
-      askedChord: getRandomElement(CHORDS),
-      lastChordCorrect: NO_ANSWER,
-      selectedChordFamilies: new Set(CHORD_FAMILIES.map(c => c.name)),
-      selectedKeys: new Set(KEYS),
-      showModal: false
-    }
 
-    this.validateChord = this.validateChord.bind(this)
-    this.nextChord = this.nextChord.bind(this)
-    this.togglePianoKey = this.togglePianoKey.bind(this)
-    this.handleChordFamilyCheckboxChanged = this.handleChordFamilyCheckboxChanged.bind(this)
-    this.handleKeyCheckboxChanged = this.handleKeyCheckboxChanged.bind(this)
-    this.closeModal = this.closeModal.bind(this)
-    this.openModal = this.openModal.bind(this)
-    this.getChordDisplay = this.getChordDisplay.bind(this)
-  }
+function App () {
 
-  handleChordFamilyCheckboxChanged(e) {
+  const [selectedPianoKeys, setSelectedPianoKeys] = useState([])
+  const [askedChord, setAskedChord] = useState(getRandomElement(CHORDS))
+  const [lastChordCorrect, setLastChordCorrect] = useState(NO_ANSWER)
+  const [selectedChordFamilies, setSelectedChordFamilies] = useState(new Set(CHORD_FAMILIES.map(c => c.name)))
+  const [selectedKeys, setSelectedKeys] = useState(new Set(KEYS))
+  const [showModal, setShowModal] = useState(false)
+
+  function handleChordFamilyCheckboxChanged(e) {
     const chordFamily = e.target.name
-    const { selectedChordFamilies } = this.state
-    if (selectedChordFamilies.has(chordFamily)) {
-      selectedChordFamilies.delete(chordFamily);
+    let selectedChordFamiliesCopy = selectedChordFamilies
+    if (selectedChordFamiliesCopy.has(chordFamily)) {
+      selectedChordFamiliesCopy.delete(chordFamily);
     } else {
-      selectedChordFamilies.add(chordFamily);
+      selectedChordFamiliesCopy.add(chordFamily);
     }
-    this.setState({ selectedChordFamilies })
+    setSelectedChordFamilies(selectedChordFamiliesCopy)
   }
 
-  handleKeyCheckboxChanged(e) {
+  function handleKeyCheckboxChanged(e) {
     const key = e.target.name
-    const { selectedKeys } = this.state
-    if (selectedKeys.has(key)) {
-      selectedKeys.delete(key);
+    let selectedKeysCopy = selectedKeys
+    if (selectedKeysCopy.has(key)) {
+      selectedKeysCopy.delete(key);
     } else {
-      selectedKeys.add(key);
+      selectedKeysCopy.add(key);
     }
-    this.setState({ selectedKeys })
+    setSelectedKeys(selectedKeysCopy)
   }
 
   // validates if the chord entered by the user is correct
-  validateChord() {
-    let { selectedPianoKeys } = this.state
-    sortByNoteOrder(selectedPianoKeys, PIANO_KEYS.map(k => k.id))
-    const correct = this.state.askedChord.notes.join() === selectedPianoKeys.join() ? CORRECT : INCORRECT
-    this.setState({
-      lastChordCorrect: correct
-    })
+  function validateChord() {
+    let selectedPianoKeysCopy = selectedPianoKeys
+    sortByNoteOrder(selectedPianoKeysCopy, PIANO_KEYS.map(k => k.id))
+    const correct = askedChord.notes.join() === selectedPianoKeysCopy.join() ? CORRECT : INCORRECT
+    setLastChordCorrect(correct)
   }
 
   // fetches a new random chord and presents it to the user
-  nextChord() {
+  function nextChord() {
     const chordsSubset = CHORDS.filter(
-      chord => this.state.selectedChordFamilies.has(chord.family.name)
+      chord => selectedChordFamilies.has(chord.family.name)
     ).filter(
-      chord => this.state.selectedKeys.has(chord.rootNote)
+      chord => selectedKeys.has(chord.rootNote)
     )
     const nextChord = getRandomElement(chordsSubset)
-    this.setState({
-      askedChord: nextChord,
-      lastChordCorrect: NO_ANSWER,
-      selectedPianoKeys: []
-    })
+    setAskedChord(nextChord)
+    setLastChordCorrect(NO_ANSWER)
+    setSelectedPianoKeys([])
   }
 
-  getChordDisplay(chord) {
+  function getChordDisplay(chord) {
     let chordDisplay = chord.rootNote
     chordDisplay += (chord.family.minor ? "m" : "")
     return <p id="asked-chord">{chordDisplay}<sup>{chord.family.extension}</sup>{chord.baseNote ? "/"+chord.baseNote : ""}</p>
   }
 
   // toggles if a piano key is selected or not
-  togglePianoKey(keyId) {
-    let { selectedPianoKeys } = this.state
-    if (!selectedPianoKeys.includes(keyId)) {
-      selectedPianoKeys.push(keyId)
+  function togglePianoKey(keyId) {
+    let selectedPianoKeysCopy = selectedPianoKeys
+    if (!selectedPianoKeysCopy.includes(keyId)) {
+      selectedPianoKeysCopy.push(keyId)
     } else {
-      removeElement(selectedPianoKeys, keyId)
+      removeElement(selectedPianoKeysCopy, keyId)
     }
-    this.setState({selectedPianoKeys})
+    setSelectedPianoKeys(selectedPianoKeysCopy)
   }
 
-  closeModal() {
-    this.setState({showModal: false})
-  }
+  return (
+    <div className="App">
+      <Modal show={this.state.showModal} onHide={this.closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Settings</Modal.Title>
+        </Modal.Header>
 
-  openModal() {
-    this.setState({showModal: true})
-  }
+        <Modal.Body>
+          <Form>
+            <p>Chord types</p>
+            {CHORD_FAMILIES.map(chordFamily => (
+              <Form.Check
+                type="switch"
+                id={chordFamily.name}
+                name={chordFamily.name}
+                label={chordFamily.name}
+                key={chordFamily.name}
+                onChange={this.handleChordFamilyCheckboxChanged}
+                checked={this.state.selectedChordFamilies.has(chordFamily.name)}
+              />
+            ))}
+            <p>Keys</p>
+            {KEYS.map(key => (
+              <Form.Check
+                type="switch"
+                id={key}
+                name={key}
+                label={key}
+                key={key}
+                onChange={this.handleKeyCheckboxChanged}
+                checked={this.state.selectedKeys.has(key)}
+              />
+            ))}
+          </Form>
+        </Modal.Body>
 
-  render() {
-    return (
-      <div className="App">
-        <Modal show={this.state.showModal} onHide={this.closeModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>Settings</Modal.Title>
-          </Modal.Header>
+        <Modal.Footer>
+          <Button onClick={setShowModal(false)} variant="primary">OK</Button>
+        </Modal.Footer>
+      </Modal>
+      
+      {this.getChordDisplay(this.state.askedChord)}
 
-          <Modal.Body>
-            <Form>
-              <p>Chord types</p>
-              {CHORD_FAMILIES.map(chordFamily => (
-                <Form.Check
-                  type="switch"
-                  id={chordFamily.name}
-                  name={chordFamily.name}
-                  label={chordFamily.name}
-                  key={chordFamily.name}
-                  onChange={this.handleChordFamilyCheckboxChanged}
-                  checked={this.state.selectedChordFamilies.has(chordFamily.name)}
-                />
-              ))}
-              <p>Keys</p>
-              {KEYS.map(key => (
-                <Form.Check
-                  type="switch"
-                  id={key}
-                  name={key}
-                  label={key}
-                  key={key}
-                  onChange={this.handleKeyCheckboxChanged}
-                  checked={this.state.selectedKeys.has(key)}
-                />
-              ))}
-            </Form>
-          </Modal.Body>
-
-          <Modal.Footer>
-            <Button onClick={this.closeModal} variant="primary">OK</Button>
-          </Modal.Footer>
-        </Modal>
-        
-        {this.getChordDisplay(this.state.askedChord)}
-
-        <div id="keyboard">
-          {PIANO_KEYS.map(pianoKey => {
-            let clazz = pianoKey.class
-            if (this.state.selectedPianoKeys.includes(pianoKey.id)) {
-              clazz += " selected"
-            } 
-            return <div 
-              className={clazz}
-              id={pianoKey.id}
-              key={pianoKey.id}
-              onClick={() => this.togglePianoKey(pianoKey.id)}
-            />
-          })}
-        </div>
-        <div id="result-container">
-
-        <p id="answer" className={this.state.lastChordCorrect === NO_ANSWER ? "invisible" : "visible"}>{this.state.lastChordCorrect}</p>
-        
-        { this.state.lastChordCorrect !== CORRECT && <Button id="validate-button" size="lg" variant="primary" onClick={this.validateChord}>Validate</Button>}
-
-        { this.state.lastChordCorrect === CORRECT && <Button variant="primary" size="lg" onClick={this.nextChord}>Next chord!</Button>}
-        </div>
-        <div>
-          <Button variant="outline-secondary" size="lg" onClick={this.openModal}>Settings</Button>
-        </div>
-
-        <Nav className="justify-content-center">
-          <Nav.Item>
-            <Nav.Link>Chord training</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>Stats</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>Settings</Nav.Link>
-          </Nav.Item>
-        </Nav>
+      <div id="keyboard">
+        {PIANO_KEYS.map(pianoKey => {
+          let clazz = pianoKey.class
+          if (this.state.selectedPianoKeys.includes(pianoKey.id)) {
+            clazz += " selected"
+          } 
+          return <div 
+            className={clazz}
+            id={pianoKey.id}
+            key={pianoKey.id}
+            onClick={() => this.togglePianoKey(pianoKey.id)}
+          />
+        })}
       </div>
-    );
-  }
+      <div id="result-container">
+
+      <p id="answer" className={this.state.lastChordCorrect === NO_ANSWER ? "invisible" : "visible"}>{this.state.lastChordCorrect}</p>
+      
+      { this.state.lastChordCorrect !== CORRECT && <Button id="validate-button" size="lg" variant="primary" onClick={this.validateChord}>Validate</Button>}
+
+      { this.state.lastChordCorrect === CORRECT && <Button variant="primary" size="lg" onClick={this.nextChord}>Next chord!</Button>}
+      </div>
+      <div>
+        <Button variant="outline-secondary" size="lg" onClick={setShowModal(true)}>Settings</Button>
+      </div>
+
+      <Nav className="justify-content-center">
+        <Nav.Item>
+          <Nav.Link>Chord training</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link>Stats</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link>Settings</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>
+  );
 }
 
 export default App;
