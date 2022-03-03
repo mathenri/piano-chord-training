b53ddb358a6f6afd6dca61ec104f24dea38b9a8e
Mattias Henriksson <mattias.henriksson@readly.com>
2022-03-03 16:00:43 +0100

    add count by extension chart

--- src/routes/stats.js
+++ src/routes/stats.js
@@ -5,11 +5,13 @@ import { Chart } from 'react-chartjs-2'
 import { Chart as ChartJS } from 'chart.js/auto'
 
 export default function Stats() {
-  const [stats, setStats] = useState([])
+  const [countByDay, setCountByDay] = useState([])
+  const [countByExtension, setCountByExtension] = useState([])
   
   useEffect(() => getStatsFromServer(), [])
 
   function getStatsFromServer() {
+    // fetch count_by_day data
     fetch(process.env.REACT_APP_BACKEND_URL_STATS + "/count_by_day", {
       method: "GET",
       headers: {
@@ -18,21 +20,33 @@ export default function Stats() {
     })
     .then(response => response.json())
     .then(data => {
-      console.log(data)
-      setStats(data)
+      setCountByDay(data)
+    })
+    .catch(err => console.log(err))
+
+    // fetch count_by_extension data
+    fetch(process.env.REACT_APP_BACKEND_URL_STATS + "/count_by_extension", {
+      method: "GET",
+      headers: {
+        'X-Auth-Token': process.env.REACT_APP_BACKEND_AUTH_TOKEN // Note: this will be visible to browser users
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      setCountByExtension(data)
     })
     .catch(err => console.log(err))
   }
 
-  function getChartData(inputData) {
-    const labels = inputData.map(d => d.day)
+  function getChartData(inputData, title, labelFieldName) {
+    const labels = inputData.map(d => d[labelFieldName])
     const data = inputData.map(d => d.count)
 
     return {
       labels,
       datasets: [
         {
-          label: 'Count by day',
+          label: title,
           backgroundColor: 'rgba(75,192,192,1)',
           data
         }
@@ -43,7 +57,8 @@ export default function Stats() {
   return (
     <main>
       <h2>Stats</h2>
-      <Bar data={getChartData(stats)} />
+      <Bar data={getChartData(countByDay, "Count by day", "day")} height={400}/>
+      <Bar data={getChartData(countByExtension, "Count by extension", "chord_extension")} height={400}/>
       <nav>
         <Link to="/piano-chord-training">Piano</Link> |{" "}
         <Link to="/chord-progressions">Chord Progressions</Link> |{" "}
